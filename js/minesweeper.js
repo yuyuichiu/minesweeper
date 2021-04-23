fd985f99db46b7fc572c7ab1e23053c49938780e
Maxwell Yu <MaxwellYu@rsmhk.com>
2021-04-23 16:41:09 +0800

    .

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -9,13 +9,23 @@ Avoid first hit
 Things to do when win and lose
 win condition
 */
+var flagMode = false;
 
 $(document).ready(function(){
     $(".mine-cell").click(function(){
         let domRowIndex = this.parentNode.rowIndex;
         let domColIndex = this.cellIndex;
 
-        myBoard.engage(domRowIndex,domColIndex);
+        if(flagMode){
+            myBoard.flag(domRowIndex,domColIndex); }
+        else{
+            myBoard.engage(domRowIndex,domColIndex); }
+    })
+
+    // Toggler between flag mode on & off.
+    $(".flag-btn").click(function(){
+        flagMode = flagMode ? false : true;
+        console.log(flagMode);
     })
 
     // Unload the loading screen
@@ -29,39 +39,59 @@ class MineField{
     }
     
     boardGenerate(vlen, hlen, mines){
-        this.vlen = vlen;
-        this.hlen = hlen;
-        this.board = [];
-        this.displayBoard = [];
-        this.revealed = [];
-        this.mines = mines || 10;
-        if(this.mines > vlen*hlen) throw "Mine exceeds Field Area."
+        this.vlen = vlen;         // vertical(y) length
+        this.hlen = hlen;         // horizontal(x) length
+        this.board = [];          // the real board with mines and numbers
+        this.displayBoard = [];   // the top layer board for display
+        this.revealed = [];       // record revealed cells
+        this.mines = mines || 10;  // mine count, default 10
+        if(this.mines > this.vlen*this.hlen) throw "Mine exceeds Field Area."
 
         // Board generation
-        for(let v = 0; v < vlen; v++){
+        for(let v = 0; v < this.vlen; v++){
             if(!(this.board[v])) // crearte subarray for rows
                 this.board[v] = [];
                 this.displayBoard[v] = [];
-            for(let h = 0; h < hlen; h++){ // create subarray col space
+            for(let h = 0; h < this.hlen; h++){ // create subarray col space
                 this.board[v][h] = " ";
                 this.displayBoard[v][h] = "_";
             }
         }
+        return this.board;
+    }
+    
+    generateMines(initialV, initialH){ // arguments = first hit position
+        // Setup immute range based of cells around initial hit
+        let immuteRadius = 1;
+        let immuteCells = [];
+        // Add affected cell position into immuteCells array
+        for(let irV = initialV - immuteRadius; irV <= initialV + immuteRadius; irV++){
+            for(let irH = initialH - immuteRadius; irH <= initialH + immuteRadius; irH++){
+                // Vaildation check for position
+                if(irV <= this.vlen && irH <= this.hlen && irV >= 0 && irH >= 0){
+                    immuteCells.push(irV + " " + irH); }
+            }
+        }
+        console.log(immuteCells);
 
         // Put mines randomly on board
         while(this.minePos.length < this.mines){
-            let randV = Math.floor(Math.random() * vlen);
-            let randH = Math.floor(Math.random() * hlen);
+            let randV = Math.floor(Math.random() * this.vlen);
+            let randH = Math.floor(Math.random() * this.hlen);
             // Put mine and record position if cell is valid
-            if(this.board[randV][randH] !== "X"){
-                this.board[randV][randH] = "X";
-                this.minePos.push(String(randV + " " + randH));
+            if(this.board[randV][randH] !== "X"){ // not a mine
+                // Check if selected cell is not in immute range
+                if(immuteCells.filter(x => x === randV + " " + randH).length === 0){
+                    // Put mine to field
+                    this.board[randV][randH] = "X";
+                    this.minePos.push(String(randV + " " + randH));
+                }   
             }
         }
-
+    
         // Add number to cell
-        for(let v = 0; v < vlen; v++){
-            for(let h = 0; h < hlen; h++){
+        for(let v = 0; v < this.vlen; v++){
+            for(let h = 0; h < this.hlen; h++){
                 // Count surrounding mines of non-mine cells
                 if(this.board[v][h] !== "X"){
                     let minecount = 0;
@@ -69,7 +99,7 @@ class MineField{
                     for(let voffset = v-1; voffset <= v+1; voffset++){
                         for(let hoffset = h-1; hoffset <= h+1; hoffset++){
                             // Skip cases with invaild cell value
-                            if(voffset > vlen-1 || hoffset > hlen-1 || voffset < 0 || hoffset < 0)
+                            if(voffset > this.vlen-1 || hoffset > this.hlen-1 || voffset < 0 || hoffset < 0)
                                 continue;
                             else if(!(this.board[voffset][hoffset]))
                                 continue;
@@ -84,9 +114,10 @@ class MineField{
             }
         }
 
-        return this.board;
+        this.boardDisplay();
+        return this.board
     }
-
+    
     boardDisplay(){
         console.log("=======Board=======");
         for(let v = 0; v < this.board.length; v++){
@@ -102,12 +133,17 @@ class MineField{
     }
 
     engage(vPos,hPos){
+        // Generate mine on first click
+        if(this.revealed.length === 0){
+            this.generateMines(vPos, hPos); }
+
         // Lose scenario
         if(this.board[vPos][hPos] == "X"){
             /* Things to do when lost */
             console.log("You lose");
             // show lose screen and ask if play again
         }
+
         // Reveal Cell
         this.reveal(vPos, hPos);
         this.topLayerBoardDisplay();
@@ -144,12 +180,19 @@ class MineField{
                 for(let hTemp = hPos - 1; hTemp <= hPos + 1; hTemp++){
                     // recursive on valid cell
                     if(vTemp < this.vlen && hTemp < this.hlen && vTemp >= 0 && hTemp >=0){
-                        console.log(vTemp + " " + hTemp);
                         this.reveal(vTemp,hTemp);
                     }
                 }
             }
         }
+    }
+
+    flag(vPos, hPos){
+        // Toggle between flagged and unflagged
+        this.displayBoard[vPos][hPos] = this.displayBoard[vPos][hPos] === "F" ? "_" : "F";
+        
+        // Disable / Enable DOM element from bring clickable
+        // and display a flag icon.
     }
 }
 
